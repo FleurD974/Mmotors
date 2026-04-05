06320e3f1b93952e6ec75f823970d38eda610b1b
FleurD974 <fleur.dambreville@gmail.com>
2026-04-05 17:43:15 +0200

    Create car model and register first car

--- store/models.py
+++ store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    brand = models.CharField(max_length=128)
+    model = models.CharField(max_length=128)
+    engine = models.CharField(max_length=55)
+    mileage = models.IntegerField(default=0)
+    passenger_number = models.IntegerField(default=2)
+    is_purchased = models.BooleanField(default=False)
+    is_leased = models.BooleanField(default=True)
+    purchase_price = models.FloatField(default=0.0)
+    leasing_price = models.FloatField(default=0.0)
+    registration_number = models.CharField(max_length=10)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f"{self.model} ({self.brand})"
+    
