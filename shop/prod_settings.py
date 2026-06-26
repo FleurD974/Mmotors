9bfb3feab225ff2e111d3fb774e716b5594acca5
FleurD974 <fleur.dambreville@gmail.com>
2026-06-26 18:41:01 +0200

    Config heroku for postgresql

--- shop/prod_settings.py
+++ shop/prod_settings.py
@@ -1,3 +1,6 @@
+import dj_database_url
+import os
+
 from .settings import *
 
 DEBUG = False
@@ -6,3 +9,5 @@ TEMPLATE_DEBUG = False
 SECRET_KEY = 'jt@(*tw+w-fm3o*kfobfhrkum!-84-h3b5=_$u(1b(9xazvcpp'
 
 ALLOWED_HOSTS = ['mmotors-project-5dec44416bdd.herokuapp.com']
+
+DATABASES['default'] = dj_database_url.config()
