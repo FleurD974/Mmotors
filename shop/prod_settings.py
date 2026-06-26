e924c5f977360905058addd85e8b5a836d7a7864
FleurD974 <fleur.dambreville@gmail.com>
2026-06-26 18:32:23 +0200

    Add production settings

---
+++ shop/prod_settings.py
@@ -0,0 +1,8 @@
+from .settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+SECRET_KEY = 'jt@(*tw+w-fm3o*kfobfhrkum!-84-h3b5=_$u(1b(9xazvcpp'
+
+ALLOWED_HOSTS = ['mmotors-project-5dec44416bdd.herokuapp.com']
