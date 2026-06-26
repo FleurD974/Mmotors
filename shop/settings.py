ab1f387b38c68a0b7f4f9d260380703cc009fbdb
FleurD974 <fleur.dambreville@gmail.com>
2026-06-26 18:06:06 +0200

    Add requirements

--- shop/settings.py
+++ shop/settings.py
@@ -152,9 +152,6 @@ LOGGING = {
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "shop" / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = (
-    'whitenoise.storage.CompressedManifestStaticFilesStorage'
-)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
